c6c89bbc8484e2addeb248380460f77348562bb3
Sande John Sparjan <65751195+SandeJohnSparjan@users.noreply.github.com>
2020-11-11 23:38:59 -0600

    add an expense class added

--- includes/init.php
+++ includes/init.php
@@ -7,6 +7,7 @@ require 'classes/user.php';
 require 'classes/friend.php';
 require 'classes/email.php';
 require 'classes/groups_all.php';
+require 'classes/expenses.php';
 
 $db_obj = new Database();
 $db_connection = $db_obj->dbConnection();
@@ -18,3 +19,5 @@ $friend_obj = new Friend($db_connection);
 $email_obj = new Email($db_connection);
 
 $group_obj = new Group($db_connection);
+
+$expenses_obj = new Expenses($db_connection);
